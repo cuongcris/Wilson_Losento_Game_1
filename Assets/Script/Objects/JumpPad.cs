c312ea022be48673cec03dc1edc4b6c67eeacbb8
Ngo Minh Cuong <91053208+cuongcris@users.noreply.github.com>
2024-03-05 16:42:16 +0700

    jumpPad

---
+++ Assets/Script/Objects/JumpPad.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPad : MonoBehaviour
+{
+    private BoxCollider2D boxCollider2D;
+    private Animator animator;
+    private void Start()
+    {
+        boxCollider2D = GetComponent<BoxCollider2D>();
+        animator = GetComponent<Animator>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (collision.contacts[0].normal.y < -0.5f)
+            {
+                animator.SetTrigger("jump");
+                collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 15f, ForceMode2D.Impulse);
+            }
+            else
+            {
+                boxCollider2D.isTrigger = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        boxCollider2D.isTrigger = false;
+    }
+}
